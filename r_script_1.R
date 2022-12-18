12019951261e3272efed661093aadc7c63be1609
zaccarogianluca <gianlucazaccaro7@gmail.com>
2022-12-18 19:34:44 +0100

    authentication

--- r_script_1.R
+++ r_script_1.R
@@ -1,9 +1,16 @@
 #connection to twitter API and get authentication
-install.packages("twitteR")
-library("twitteR")
+install.packages("rtweet")
+library(rtweet)
 
-consumerKey=’XXXXXXXXXXXXXXXXXXXXXXXX’
-consumerSecret=’XXXXXXXXXXXXXXXXXXXXXX’accessToken=’XXXXXXXXXXXXXXXXXXXX’
-accessTokenSecret= ‘XXXXXXXXXXXXXXXXXXXXXXXXXX’
-setup_twitter_oauth(consumerKey,consumerSecret,accessToken,accessTokenSecret)
+token <- create_token(
+  app = "Giandatascience",
+  consumer_key = "yLmH1AhOpL0rXhhsXgrqjNZ1m",
+  consumer_secret = "dSsdHIm6Vjej1rmANi8Fv9LeZWGmA4jeycoixGERvd5zNPtHMK",
+  access_token = "1603835172675256331-aqS2fFo8O2otGTSXlAYP1OcckQCSvB",
+  access_secret = "r6mXeLqURQTDgFUo7vRXy5RVmB0UQpXlq6L3lZ2WhSKWY",
+)
 
+aut1 <- rtweet_app()
+auth_save(aut1, "GianZac")
+
+auth_as("GianZac")
