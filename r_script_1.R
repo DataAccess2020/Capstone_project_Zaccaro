75fc5b4192220757fa9f29e4a5c1cfd913cbf3af
zaccarogianluca <gianlucazaccaro7@gmail.com>
2022-12-18 19:41:35 +0100

    authentication

--- r_script_1.R
+++ r_script_1.R
@@ -4,10 +4,10 @@ library(rtweet)
 
 token <- create_token(
   app = "Giandatascience",
-  consumer_key = "yLmH1AhOpL0rXhhsXgrqjNZ1m",
-  consumer_secret = "dSsdHIm6Vjej1rmANi8Fv9LeZWGmA4jeycoixGERvd5zNPtHMK",
-  access_token = "1603835172675256331-aqS2fFo8O2otGTSXlAYP1OcckQCSvB",
-  access_secret = "r6mXeLqURQTDgFUo7vRXy5RVmB0UQpXlq6L3lZ2WhSKWY",
+  consumer_key = "xx",
+  consumer_secret = "xx",
+  access_token = "xx",
+  access_secret = "xx",
 )
 
 aut1 <- rtweet_app()
